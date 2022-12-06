e82291688faaf78abc373150794a8edb01b512b9
Daniel-Alencar <danielalencar746@gmail.com>
2022-12-06 18:08:53 -0300

    funcionando

--- 2-analisar_vehicles.py
+++ 2-analisar_vehicles.py
@@ -1,8 +1,13 @@
 from Vehicle_Search import Vehicle_Search
 import util
+import json
 
 vehicles = util.read_json("json/vehicles.json")
 util.print_formatted_json(vehicles)
+
+def update_json(jsonPath, jsonObject):
+  with open(jsonPath, "w") as jsonFile:
+    json.dump(jsonObject, jsonFile, indent=2)
 
 mes_busca = "setembro"
 ano_busca = 2019
@@ -19,11 +24,21 @@ categories_names = [
   "Picape média"
 ]
 
+vehicles_to_search = []
+
 for i, category in enumerate(vehicles["vehicles"]):
   list_vehicles = category[categories_names[i]]
 
   for vehicle in list_vehicles:
+    vehicle_json = {}
+
     vs = Vehicle_Search(vehicle["marca"], vehicle["modelo"], mes_busca, ano_busca)
     vehicle_names = vs.execution()
 
-    util.print_formatted_json(vehicle_names)
+    vehicle_json["marca"] = vehicle["marca"]
+    vehicle_json["modelos_base"] = []
+    vehicle_json["modelos_base"].append(vehicle_names)
+
+    vehicles_to_search.append(vehicle_json)
+
+    util.print_formatted_json(vehicles_to_search)
