e094761927b79b8526e03dc83047e787b31d6113
Daniel-Alencar <danielalencar746@gmail.com>
2022-12-19 15:08:32 -0300

    UPDATE

--- settings.py
+++ settings.py
@@ -3,23 +3,22 @@ verbose = True
 headless = False
 
 # NÃO PODEM MUDAR
-number_of_computers = 2
+number_of_computers = 5
 number_of_years = 1
 
 # PODEM MUDAR
 
 # Começa em 0
-computer_id = 1
-mini_batch = 2
+computer_id = 0
+mini_batch = 8
 
 meses = [
   "janeiro", "fevereiro", "março", "abril", "maio", "junho",
-  "julho", "agosto", "setembro", "outubro", "novembro"
+  "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
 ]
 
 anos_modelo = [
-  2020,
-  2021
+  2015, 2016, 2017, 2018, 2019, 2020
 ]
 
 structure_columns = ['Marca', 'Modelo', 'Ano-modelo']
